8c5dec02ed3e86ba628541ae74da6c6feff10c3b
XI2474 DeepikaSelvaraj <deepika.selvaraj@xebia.com>
2023-02-20 18:50:50 +0530

    dob feature

--- dms/src/main/java/com/salam/dms/model/request/CustomerProfileRequest.java
+++ dms/src/main/java/com/salam/dms/model/request/CustomerProfileRequest.java
@@ -4,6 +4,7 @@ package com.salam.dms.model.request;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 public class CustomerProfileRequest {
@@ -15,4 +16,8 @@ public class CustomerProfileRequest {
     private String mobile;
     @NotEmpty(message = "{com.constraint.FieldEmpty.message}")
     private String odbPlateNumber;
+    @NotEmpty(message = "{com.constraint.FieldEmpty.message}")
+    @Pattern(regexp = "^(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[012])-[0-9]{4}$",
+    message = "{com.constraint.DateValidation.message}")
+    private String dob;
 }
